8dd8501788826d0a866802e3f16d5d779f2d2189
Welisson luca <welissonluca17@gmail.com>
2022-03-12 21:23:35 -0300

    feat(container-di): register reports repository interface

--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -2,5 +2,12 @@ import { container } from 'tsyringe';
 
 import { UserRepository } from '@modules/accounts/infra/typeorm/repositories/UserRepository';
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository';
+import { ReportRepository } from '@modules/reports/infra/typeorm/repositories/ReportRepository';
+import { IReportRepository } from '@modules/reports/repositories/IReportRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<IReportRepository>(
+  'ReportRepository',
+  ReportRepository
+);
