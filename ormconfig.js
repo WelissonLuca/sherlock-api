ca6ba75084e0be21ff2e940214c6c2decf9a9245
Welisson luca <welissonluca17@gmail.com>
2022-03-13 13:31:55 -0300

    chore: prepare to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,24 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DB_URL,
-  migrations: ['./src/shared/infra/typeorm/migrations/**.ts'],
-  entities: ['./src/modules/*/infra/typeorm/entities/**.ts'],
+  migrations: [
+    process.env.NODE_ENV === 'develop'
+      ? './src/shared/infra/typeorm/migrations/**.ts'
+      : './dist/shared/infra/typeorm/migrations/**.js',
+  ],
+  entities: [
+    process.env.NODE_ENV === 'develop'
+      ? './src/modules/*/infra/typeorm/entities/**.ts'
+      : './dist/modules/*/infra/typeorm/entities/**.js',
+  ],
   cli: {
-    migrationsDir: './src/shared/infra/typeorm/migrations',
-    entities: './src/modules/*/infra/typeorm/entities/**.ts',
+    migrationsDir:
+      process.env.NODE_ENV === 'develop'
+        ? './src/shared/infra/typeorm/migrations'
+        : './dist/shared/infra/typeorm/migrations',
+    entities:
+      process.env.NODE_ENV === 'develop'
+        ? './src/modules/*/infra/typeorm/entities/'
+        : './dist/modules/*/infra/typeorm/entities/',
   },
 };
